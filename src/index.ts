3a1cb24be0d9f7c514e47f8d16a888f7c7c8bf2e
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-02-21 22:00:33 +0100

    Create a default endpoint to display Hello world

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const HOST = process.env.HOST || 'http://localhost';
 const PORT = parseInt(process.env.PORT || '4500');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello World!' });
+});
+
 app.listen(PORT, () => {
-  console.log(`Application started on port ${PORT} 🎉`);
+  console.log(`Application started on URL ${HOST}:${PORT} 🎉`);
 });
